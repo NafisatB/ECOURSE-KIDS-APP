f89c37802c5347b9fa7b06040a3a70b265e113ab
NafisatB <nafisatbabamusa@outlook.com>
2026-01-02 19:27:45 +0100

    completed the about us page

--- src/component/Button/Button.jsx
+++ src/component/Button/Button.jsx
@@ -1,12 +1,18 @@
 import './Button.css';
 
-export function Button({ text, variant = "primary", className = "", ...props }) {
+export function Button({ text, variant = "primary", rightIcon, className = "", ...props }) {
     return (
         <button
             className={`btn ${variant} ${className}`}
             {...props}
         >
-            {text}
+            <span className="btn__text">{text}</span>
+
+            {rightIcon && (
+                <span className="btn__icon btn__icon--right">
+                    {rightIcon}
+                </span>
+            )}
         </button>
     );
 }
