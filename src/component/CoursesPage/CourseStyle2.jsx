ab08dd086586181ad518bde883cb9eeb0c488ae2
NafisatB <nafisatbabamusa@outlook.com>
2026-01-07 01:11:51 +0100

    completed course style 2 page

--- src/component/CoursesPage/CourseStyle2.jsx
+++ src/component/CoursesPage/CourseStyle2.jsx
@@ -1,16 +1,15 @@
 import { CourseCard } from '../../component/Card/Course/CourseCard'
-import CourseImg6 from '../../assets/images/course-img6.svg'
-import CourseImg7 from '../../assets/images/course-img7.svg'
-import CourseImg8 from '../../assets/images/course-img8.svg'
-import CourseImg9 from '../../assets/images/course-img9.svg'
-import CourseImg10 from '../../assets/images/course-img10.svg'
-import CourseInstructor1 from '../../assets/images/course-instructor1.svg'
-import CourseInstructor2 from '../../assets/images/course-instructor2.svg'
-import CourseInstructor3 from '../../assets/images/course-instructor3.svg'
+import CourseImg11 from '../../assets/images/course-img11.svg'
+import CourseImg12 from '../../assets/images/course-img12.svg'
+import CourseImg13 from '../../assets/images/course-img13.svg'
+import CourseImg14 from '../../assets/images/course-img14.svg'
+import CourseImg15 from '../../assets/images/course-img15.svg'
+import CourseImg16 from '../../assets/images/course-img16.svg'
+import CourseInstructor4 from '../../assets/images/course-instructor4.svg'
 
-import { CoursesHeader } from '../../component/AboutPageSection/Courses/CourseHeader';
 
-export function Style1() {
+
+export function Style2() {
     const handleEnroll = () => {
         console.log('User enrolled!');
     };
@@ -21,20 +20,12 @@ export function Style1() {
 
     return (
         <div className='pt-40 max-w-[1350px] mx-auto gap-15 font-sora mb-20 relative px-5 lg:px-0'>
-            <CoursesHeader
-                label="TOP POPULAR COURSE"
-                title="Edunity Course Student Can Join With Us"
-                buttonText="Load More Course"
-                buttonVariant="purple-btn"
-                buttonRightIcon="→"
-                onButtonClick={() => navigate('/courses')}
-            />
+         
 
-            <div style={{ padding: '2rem 1rem', }}>
+            <div >
                 <div className=" grid grid-cols-[repeat(auto-fill,minmax(380px,1fr))] gap-8">
                     <CourseCard
-                        image={CourseImg6}
-                        badge="Digital Marketing"
+                        image={CourseImg11}
                         title="It Statistics Data Science And Business Analysis"
                         rating={5}
                         ratingCount={4.5}
@@ -42,14 +33,13 @@ export function Style1() {
                         lessonNumber={10}
                         duration="19h 30m"
                         studentCount="20+"
-                        instructor="Samantha"
-                        instructorAvatar={CourseInstructor1}
+                        instructor="By Angela in Development"
+                        instructorAvatar={CourseInstructor4}
                         onEnroll={handleEnroll}
                     />
 
                     <CourseCard
-                        image={CourseImg7}
-                        badge="Digital Marketing"
+                        image={CourseImg12}
                         title="Beginner Adobe Illustrator for Graphic Design"
                         rating={4}
                         ratingCount={4.5}
@@ -57,14 +47,13 @@ export function Style1() {
                         lessonNumber={10}
                         duration="19h 30m"
                         studentCount="20+"
-                        instructor="Charles"
-                        instructorAvatar={CourseInstructor2}
+                        instructor="By Angela in Development"
+                        instructorAvatar={CourseInstructor4}
                         onEnroll={handleEnroll}
                     />
 
                     <CourseCard
-                        image={CourseImg8}
-                        badge="Digital Marketing"
+                        image={CourseImg13}
                         title="Starting SEO As Your Home Based Business"
                         rating={5}
                         ratingCount={4.5}
@@ -72,13 +61,12 @@ export function Style1() {
                         lessonNumber={10}
                         duration="19h 30m"
                         studentCount="20+"
-                        instructor="Morgan"
-                        instructorAvatar={CourseInstructor3}
+                        instructor="By Angela in Development"
+                        instructorAvatar={CourseInstructor4}
                     />
 
                     <CourseCard
-                        image={CourseImg8}
-                        badge="Digital Marketing"
+                        image={CourseImg14}
                         title="Beginner Adobe Illustrator for Graphic Design"
                         rating={4}
                         ratingCount={4.5}
@@ -86,13 +74,13 @@ export function Style1() {
                         lessonNumber={10}
                         duration="19h 30m"
                         studentCount="20+"
-                        instructor="Brian Brower"
-                        instructorAvatar={CourseInstructor2}
+                        instructor="By Angela in Development"
+                        instructorAvatar={CourseInstructor4}
                         onEnroll={handleEnroll}
                     />
 
                     <CourseCard
-                        image={CourseImg9}
+                        image={CourseImg15}
                         badge="Digital Marketing"
                         title="It Statistics Data Science And Business Analysis"
                         rating={4}
@@ -101,14 +89,13 @@ export function Style1() {
                         lessonNumber={10}
                         duration="19h 30m"
                         studentCount="20+"
-                        instructor="Rodriquez"
-                        instructorAvatar={CourseInstructor2}
+                        instructor="By Angela in Development"
+                        instructorAvatar={CourseInstructor4}
                         onEnroll={handleEnroll}
                     />
 
                     <CourseCard
-                        image={CourseImg10}
-                        badge="Digital Marketing"
+                        image={CourseImg16}
                         title="Starting SEO As Your Home Based Business"
                         rating={4}
                         ratingCount={4.5}
@@ -116,8 +103,8 @@ export function Style1() {
                         lessonNumber={10}
                         duration="19h 30m"
                         studentCount="20+"
-                        instructor="Morgan"
-                        instructorAvatar={CourseInstructor2}
+                        instructor="By Angela in Development"
+                        instructorAvatar={CourseInstructor4}
                         onEnroll={handleEnroll}
                     />
 
--- src/pages/CourseStyle2.jsx
+++ src/pages/CourseStyle2.jsx
@@ -1,18 +1,18 @@
 import { PageHeaderAbtUs } from '../component/LandingPageSection/PageHeader/PageHeaderAbtUs'
 import { SubHeader } from '../component/Card/SubHeader/SubHeader'
-import { Style1 } from '../component/CoursesPage/CourseStyle1'
+import { Style2 } from '../component/CoursesPage/CourseStyle2'
 import { Footer } from '../component/AboutPageSection/Footer/Footer'
 import FooterBg from '../assets/images/footer.jpg'
 export function CourseStyle2() {
     return (
-        <div className="relative md:max-w-[1920px] mx-auto overflow-x-hidden">
+        <div className="relative bg-bleached-silk md:max-w-[1920px] mx-auto overflow-x-hidden">
             <header>
                 <PageHeaderAbtUs />
             </header>
 
             <main>
                 <SubHeader
-                    title="COURSE DETAILS"
+                    title="COURSE STYLE 2"
                     breadcrumb={
                         <>
                             <span>Home</span>
@@ -20,7 +20,7 @@ export function CourseStyle2() {
                             <span>Course</span>
                         </>
                     } />
-                <Style1/>
+                <Style2/>
             </main>
             <footer id="contact" style={{ backgroundImage: `url(${FooterBg})` }} className="bg-cover bg-center">
                 <Footer />
